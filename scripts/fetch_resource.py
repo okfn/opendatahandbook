06ac7e983cc5246f5e36a5bb6059d5ac1fc7c520
joetsoi <joe.yeung.tsoi@gmail.com>
2015-04-27 10:09:07 +0100

    [open data resources]
    make resources a collection
    move script to /scripts
    javascript filter for side menu

--- scripts/fetch_resource.py
+++ scripts/fetch_resource.py
@@ -1,6 +1,7 @@
 import csv
-import yaml
 import requests
+from slugify import slugify
+import yaml
 
 
 spreadsheet_url = 'https://docs.google.com/a/okfn.org/spreadsheets/d/1MVVxdfZJyW-WRxKMSPC3h6A13CdkycrHFwRkJcV7Og4/export?gid=0&format=csv'
@@ -13,11 +14,15 @@ def titleize(string):
 if __name__ == '__main__':
     data = []
     r = requests.get(spreadsheet_url)
-    for  row in csv.DictReader(r.iter_lines()):
+    for i, row in enumerate(csv.DictReader(r.iter_lines())):
         extra = row.pop('', None)
         if extra:
             row['Notes'] = ' '.join([row['Notes'],  extra])
         data.append(row)
 
-    with open('_data/resources.yaml', 'w') as topic_file:
-        yaml.dump(data, topic_file, default_flow_style=False)
+        title = slugify(unicode(row['Title']))
+        file_name ='_resources/{0}-{1}.yaml'
+        with open(file_name.format(i, title), 'w') as resource_file:
+            resource_file.write('---\n')
+            resource_file.write(yaml.dump(row, default_flow_style=False))
+            resource_file.write('---')
