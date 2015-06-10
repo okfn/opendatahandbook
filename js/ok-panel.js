b3b6690e25646cc3d882ca702daae229ebc3bc67
Sam Smith <sam@mintcanary.com>
2015-06-10 18:35:28 +0100

    remove ok panel js

--- js/ok-panel.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $( "a.ok-ribbon" ).addClass( "closed" ).removeAttr('href');
-    
-    $('a.ok-ribbon').click(function(e) {
-        $(this).toggleClass("closed open");
-        $('body > #ok-panel').toggleClass("closed open");
-    });
-});
