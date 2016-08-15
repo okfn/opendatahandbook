45e451920763fa9aedb66e3d21b965130f88a08f
Sam Smith <sam@mintcanary.com>
2016-08-15 16:04:05 +0100

    solutions hashchange

--- js/apps.js
+++ js/apps.js
@@ -1,50 +1,70 @@
 $(document).ready(function() {
-   $( function() {
-    // quick search regex
+
+  function getHashFilter() {
+    var hash = location.hash;
+    // get filter=filterName
+    var matches = location.hash.match( /filter=([^&]+)/i );
+    var hashFilter = matches && matches[1];
+    return hashFilter && decodeURIComponent( hashFilter );
+  }
+
+  $( function() {
+
+    var $container = $('section.apps > ul');
     var qsRegex;
-    var buttonFilter;
-    
-    // init Isotope
-    var $container = $('section.apps > ul').isotope({
-      itemSelector: 'section.apps > ul > li',
-      layoutMode: 'fitRows',
-      filter: function() {
-        var $this = $(this);
-        var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
-        var buttonResult = buttonFilter ? $this.is( buttonFilter ) : true;
-        return searchResult && buttonResult;
+
+    // bind filter button click
+    var $filters = $('.filter').on( 'click', 'a', function() {
+      var filterAttr = $( this ).attr('data-filter');
+      // set filter in hash
+      if ( filterAttr ) {
+        hashValue = filterAttr;
       }
+      else {
+        hashValue = '*';
+      }
+      location.hash = 'filter=' + encodeURIComponent( hashValue );
     });
-  
-    $('.filter').on( 'click', 'a', function() {
-      buttonFilter = $( this ).attr('data-filter');
-      $container.isotope();
-    });
-    
+
     // use value of search field to filter
     var $quicksearch = $('.quicksearch').keyup( debounce( function() {
       qsRegex = new RegExp( $quicksearch.val(), 'gi' );
       $container.isotope();
     }) );
-  
-    
-      // change is-checked class on buttons
-    $('.filter').each( function( i, buttonGroup ) {
-      var $buttonGroup = $( buttonGroup );
-      $buttonGroup.on( 'click', 'a', function() {
-        $('.app-sub-menu').find('.filter-items .active').removeClass('active');
-        $( this ).addClass('active');
 
-        //active classes for overviews
-        $('.overviews').find('.overview.active').removeClass('active');
-        $('.overviews').find($(this).data("pid")).addClass('active');
+    var isIsotopeInit = false;
 
+    function onHashchange() {
+      var hashFilter = getHashFilter();
+      if ( !hashFilter && isIsotopeInit ) {
+        return;
+      }
+
+      isIsotopeInit = true;
+      // filter isotope
+      $container.isotope({
+        itemSelector: 'section.apps > ul > li',
+        layoutMode: 'fitRows',
+        filter: function() {
+          var $this = $(this);
+          var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
+          var hashResult = hashFilter ? $this.is( hashFilter ) : true;
+          return searchResult && hashResult;
+        }
       });
-    });
-    
-    
+      // set selected class on button
+      if ( hashFilter ) {
+        $filters.find('.active').removeClass('active');
+        $filters.find('[data-filter="' + hashFilter + '"]').addClass('active');
+      }
+    }
+
+    $(window).on( 'hashchange', onHashchange );
+    // trigger event handler to init Isotope
+    onHashchange();
   });
-  
+
+
   // debounce so filtering doesn't happen every millisecond
   function debounce( fn, threshold ) {
     var timeout;
